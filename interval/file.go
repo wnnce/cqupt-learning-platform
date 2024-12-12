81f96947353c5dcb1c6303578f175180ea0a9339
CrushCola <ranchangxin@gmail.com>
2024-12-12 12:38:48 +0800

    修复文件夹不存在题库保存失败

--- interval/file.go
+++ interval/file.go
@@ -14,16 +14,22 @@ type StorageTopic struct {
 	Answer  string `json:"answer"`
 }
 
-const filePath = "./data/banks.json"
+const (
+	fileFolder = "./data/"
+	fileName   = "banks.json"
+)
 
 // ReadAnswerFile 读取保存在本地的题库文件
 func ReadAnswerFile() map[string]*StorageTopic {
-	readFile, err := os.ReadFile(filePath)
-	if err != nil {
+	readFile, err := os.ReadFile(fileFolder + fileName)
+	banks := make(map[string]*StorageTopic)
+	if os.IsNotExist(err) {
+		log.Println("题库文件不存在，返回空题库!")
+		return banks
+	} else if err != nil {
 		log.Printf("读取题库文件错误，message: %s", err)
 		return nil
 	}
-	banks := make(map[string]*StorageTopic)
 	if err = json.Unmarshal(readFile, &banks); err != nil {
 		log.Printf("格式化题库文件错误，message: %s", err)
 		return nil
@@ -35,8 +41,18 @@ func ReadAnswerFile() map[string]*StorageTopic {
 func SaveAnswersToFile(banks map[string]*StorageTopic) {
 	// 序列化为阅读友好的json格式
 	jsonValue, _ := json.MarshalIndent(banks, "", "  ")
-	if err := os.WriteFile(filePath, jsonValue, 0644); err != nil {
+	saveFolder := fileFolder
+	_, err := os.Stat(saveFolder)
+	if os.IsNotExist(err) {
+		if err = os.MkdirAll(fileFolder, 0755); err != nil {
+			log.Printf("创建 %s 文件夹失败，请手动创建，已经题库文件保存到当前目录!", fileFolder)
+			saveFolder = ""
+		}
+	}
+	// 最终的保存路径
+	finalSavePath := saveFolder + fileName
+	if err := os.WriteFile(finalSavePath, jsonValue, 0644); err != nil {
 		log.Printf("写入题库到文件错误，message: %s", err)
 	}
-	log.Printf("题库写入到题库成功，Path: %s", filePath)
+	log.Printf("题库写入到题库成功，Path: %s", finalSavePath)
 }
